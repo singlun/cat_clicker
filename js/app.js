050f693db87724d1c08bf00a5d1c5ee12a094c13
Tang Sing Lun <cokelight@gmail.com>
2018-12-10 11:48:28 +0800

    feat: Added and modify some of the new features
    feat added: getCurrentLoc
    feat modify: getCat, changeImage, increaseCounter, catListView -> init
                 catListView -> render, catView -> init, catview -> render

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ $(function() {
         addCat: function(cat_name) {
             var obj = new dataCat();            
             var thisID = dataCatObjArray.length;
-            console.log("right here");
             obj.id = thisID;
             obj.counter = 0;
             obj.cats = 'img/cat' + thisID + '.jpg';
@@ -27,20 +26,21 @@ $(function() {
             return dataCatObjArray;           
         }, 
         
-        getCat: function() {
+        getCat: function(currentPosition) {
             return dataCatObjArray[currentPosition];           
-        },         
+        }, 
+        
+        getCurrentLoc: function() {
+            return currentPosition;           
+        },        
         
         changeImage: function(cat_id) {
-            alert('here');
             currentPosition = cat_id; 
             catView.render();
-            return dataCatObjArray;
         }, 
         
         increaseCounter: function(cat) {
-            cat.counter = counter + 1;
-            return cat.counter;
+            cat.counter = cat.counter + 1;
         }, 
 
         init: function() {
@@ -70,52 +70,59 @@ $(function() {
             this.$catLists = $('#cat_lists');
 
             this.render();
+
+            
         },
 
         render: function(){
             var htmlStr = '';
             octopus.getCatLists().forEach(function(cat){
-
-                htmlStr += '<li><a onclick="changeImage(' + cat.id + ');" href="#">'+
+                htmlStr += '<li><a id="link-' + cat.id + '" href="#">'+
                     cat.cats_name +
                     '</a></li>';
-                // htmlStr += '<li><a onclick="octopus.changeImage(${cat.id});" href="#">${cat.cats_name}</a></li>';
             });
             this.$catLists.html( htmlStr );
+
+            this.$catLists.on('click',function(e) {                
+                var cat_id = parseInt(event.target.id.slice(5));
+                octopus.changeImage(cat_id);
+            });             
         }
     };
 
     var catView = {
         init: function() {
             // grab elements and html for using in the render function
-            // this.$cat = $('#img-' + dataCatObjArray[currentPosition].id);
+            var currentPos = octopus.getCurrentLoc();
+            var cat = octopus.getCat(currentPos);
             this.$cat_view = $('#section_left');
             this.catTemplate = $('script[data-template="cat"]').html();          
-            this.render();
-            this.$cat = $('#img-0');
-            // Delegated event to listen for removal clicks
-            this.$cat.on('click',function(e) {
-                var cat = $(this).parents('.cat').data();
-                console.log(cat);
-                // octopus.increaseCounter(cat);
-                // return false;
-            });              
+            this.render();            
         },
 
         render: function() {
             // Cache vars for use in forEach() callback (performance)
             var $cat_view = this.$cat_view,
-                catTemplate = this.catTemplate;
+                catTemplate = this.catTemplate,
+                currentPos = octopus.getCurrentLoc();
 
             // Clear and render
             $cat_view.html('');
-            var cat = octopus.getCat();
+            var cat = octopus.getCat(currentPos);
             // Replace template markers with data
             var thisTemplate = catTemplate.replace(/{{cat_name}}/g, cat.cats_name);
             thisTemplate = thisTemplate.replace(/{{counter}}/g, cat.counter);
             thisTemplate = thisTemplate.replace(/{{cat_img}}/g, cat.cats);
             thisTemplate = thisTemplate.replace(/{{id}}/g, cat.id);
             $cat_view.append(thisTemplate);
+            this.$catImg = $('#img-' + cat.id); 
+            this.$catSpan = $('#span-' + cat.id);                        
+            // Delegated event to listen for removal clicks
+            this.$catImg.on('click',function(e) {
+                var cat_id = $(this).parents('.cat').data();
+                octopus.increaseCounter(cat);
+                document.querySelector('#span-' + cat.id).textContent = `The cat click ${cat.counter} times`;               
+            });              
         }
     };    
 
